e5d53ecf9e74ec254130f45b6b3f1358c28f9b21
gianni <gianni.deparday@epita.fr>
2023-12-11 21:59:28 +0100

    api test six

--- src/main/java/fr/lernejo/navy_batle/Launcher.java
+++ src/main/java/fr/lernejo/navy_batle/Launcher.java
@@ -12,11 +12,13 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class Launcher {
-
     public static void main(String[] args) {
         int port = Integer.parseInt(args[0]);
         try {
-            HttpServer server = setupHttpServer(port);
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+            server.setExecutor(Executors.newFixedThreadPool(1));
+            server.createContext("/ping", new PingHandler());
+            server.createContext("/api/game/start", new GameStartHandler(port));
             server.start();
             System.out.println("Server started on port " + port);
         } catch (Exception e) {
@@ -24,51 +26,33 @@ public class Launcher {
         }
     }
 
-    private static HttpServer setupHttpServer(int port) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.setExecutor(Executors.newFixedThreadPool(1));
-        setupPingContext(server);
-        setupGameStartContext(server, port);
-        return server;
-    }
-
-    private static void setupPingContext(HttpServer server) {
-        server.createContext("/ping", (exchange -> {
+    static class PingHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
             String response = "OK";
             exchange.sendResponseHeaders(200, response.length());
             try (OutputStream os = exchange.getResponseBody()) {
                 os.write(response.getBytes());
             }
-        }));
-    }
-
-    private static void setupGameStartContext(HttpServer server, int port) {
-        server.createContext("/api/game/start", new GameStartHandler(port));
+        }
     }
 
     static class GameStartHandler implements HttpHandler {
         private final int port;
-
-        public GameStartHandler(int port) {
-            this.port = port;
-        }
-
+        public GameStartHandler(int port) { this.port = port; }
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             if (!"POST".equals(exchange.getRequestMethod())) {
                 sendResponse(exchange, 404, "");
                 return;
             }
-
-            String requestBody = getRequestBody(exchange);
-            if (requestBody == null || !isValidJson(requestBody)) {
+            String requestBody = new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
+            if (requestBody.isEmpty() || !isValidJson(requestBody)) {
                 sendResponse(exchange, 400, ""); // Bad Request for empty or malformed JSON
                 return;
             }
-
             sendResponseWithServerDetails(exchange);
         }
-
         private void sendResponse(HttpExchange exchange, int statusCode, String response) throws IOException {
             exchange.sendResponseHeaders(statusCode, response.length());
             try (OutputStream os = exchange.getResponseBody()) {
@@ -77,31 +61,20 @@ public class Launcher {
                 exchange.close();
             }
         }
-
-        private String getRequestBody(HttpExchange exchange) throws IOException {
-            try (InputStream is = exchange.getRequestBody()) {
-                return new String(is.readAllBytes(), StandardCharsets.UTF_8);
-            }
-        }
-
         private boolean isValidJson(String json) {
-            JSONParser parser = new JSONParser();
             try {
-                parser.parse(json);
+                new JSONParser().parse(json);
                 return true;
             } catch (ParseException e) {
                 return false;
             }
         }
-
         private void sendResponseWithServerDetails(HttpExchange exchange) throws IOException {
             JSONObject responseJson = new JSONObject();
             responseJson.put("id", "some-id");
             responseJson.put("url", "http://localhost:" + port);
             responseJson.put("message", "May the best code win");
-
-            String response = responseJson.toJSONString();
-            sendResponse(exchange, 202, response);
+            sendResponse(exchange, 202, responseJson.toJSONString());
         }
     }
 }
