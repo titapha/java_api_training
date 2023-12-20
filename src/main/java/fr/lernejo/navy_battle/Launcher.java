91f4790241ce142c36c30b05f83a3b51614d433b
gianni <gianni.deparday@epita.fr>
2023-12-20 16:42:51 +0100

    test pitie

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,43 +1,57 @@
 package fr.lernejo.navy_battle;
 
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
-import java.io.IOException;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class Launcher {
     private final int port;
     private final String adversaryUrl;
-    private final HttpServer server;
 
-    public Launcher(int port, String adversaryUrl, HttpServer server) {
+    public Launcher(int port, String adversaryUrl) {
         this.port = port;
         this.adversaryUrl = adversaryUrl;
-        this.server = server;
     }
 
-    public void startServer() throws IOException {
-        server.bind(new InetSocketAddress(port), 0);
-        server.setExecutor(Executors.newFixedThreadPool(1));
-        server.createContext("/ping", new PingHandler());
-        server.createContext("/api/game/start", new GameStartHandler(port));
-        server.createContext("/api/game/fire", new FireHandler());
-        server.start();
+    public void startServer() {
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+            server.setExecutor(Executors.newFixedThreadPool(1));
+            server.createContext("/ping", new PingHandler());
+            server.createContext("/api/game/start", new GameStartHandler(port));
+            server.start();
+            System.out.println("Server started on port " + port);
 
-        System.out.println("Server started on port " + port);
-
-        if (adversaryUrl != null) {
-            new AdversaryContact(adversaryUrl, port).sendPostRequest();
+            if (adversaryUrl != null) {
+                new AdversaryContact(adversaryUrl, port).sendPostRequest();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Usage: java Launcher <port> [adversaryUrl]");
+            return;
+        }
+        int port = Integer.parseInt(args[0]);
+        String adversaryUrl = args.length > 1 ? args[1] : null;
+        new Launcher(port, adversaryUrl).startServer();
     }
 
     static class PingHandler implements HttpHandler {
         @Override
-        public void handle(com.sun.net.httpserver.HttpExchange exchange) throws IOException {
-            // PingHandler implementation
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "OK";
+            exchange.sendResponseHeaders(200, response.length());
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
         }
     }
-
-    // Include other inner classes like GameStartHandler, FireHandler, etc.
 }
